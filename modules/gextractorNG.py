60d80f5c530c1f023bdf60e015084688eb715e6a
MultiElio <eliov@hotmail.fr>
2020-02-12 16:49:45 +0100

    new way to calculate position of objects

--- modules/gextractorNG.py
+++ modules/gextractorNG.py
@@ -32,7 +32,7 @@ class NGExtractors(object):
         difference = cv2.absdiff(gray, self.back)
 
         # Apply thresholding to eliminate noise
-        thresh = cv2.threshold(difference, 20, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.threshold(difference, 30, 255, cv2.THRESH_BINARY)[1]
         thresh = cv2.dilate(thresh, None, iterations=2)
         res = cv2.bitwise_and(img,img, mask= thresh)
 
@@ -52,14 +52,14 @@ class NGExtractors(object):
             cv2.drawContours(res, [box.astype("int")], -1, (0, 255, 0), 2)
             cX = np.average(box[:, 0])
             cY = np.average(box[:, 1]) 
-
+            bas_y = np.max(box[:, 1]) 
             colors = (img[int(cY),int(cX)])
             color = colors[1]>colors[2]
             robot = 0
             robot_area = conf.size_min_robot
             if(cv2.contourArea(c)>robot_area):
                 robot = 1
-            data.log(Point(cX,cY),color,robot)
+            data.log(Point(cX,bas_y),color,robot)
         #cv2.drawContours(res, cnts, -1, (255,0,0), 3)
         #cv2.imshow('mask + track', res)
         return res
